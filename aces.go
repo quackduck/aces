bc5d6c348c5d90d89adfedeb1494c888cae18972
Ishan Goel <igoel.mail@gmail.com>
2022-11-26 13:22:46 -0800

    Use the right-sized types for everything

--- aces.go
+++ aces.go
@@ -8,25 +8,26 @@ import (
 	"math"
 )
 
-var BufSize = 16 * 1024
+var BufSize int = 16 * 1024
 
 // sliceByteLen slices the byte b such that the result has length len and starting bit start
-func sliceByteLen(b byte, start int, len int) byte {
-	return (b << start) >> byte(8-len)
+func sliceByteLen(b byte, start uint8, len uint8) byte {
+	return (b << start) >> (8 - len)
 }
 
 type BitReader struct {
-	chunkLen int
+	chunkLen uint8
 	in       io.Reader
 
-	buf    []byte
-	bitIdx int
-	bufN   int
+	buf     []byte
+	bitIdx  uint8
+	byteIdx int
+	bufN    int // limited by read size which cannot exceed an int
 }
 
-func NewBitReader(chunkLen int, in io.Reader) (*BitReader, error) {
-	//bufSize % bs.chunkLen == 0 so that we never have to read across the buffer boundary
-	bs := &BitReader{chunkLen: chunkLen, in: in, buf: make([]byte, BufSize-BufSize%chunkLen)}
+func NewBitReader(chunkLen uint8, in io.Reader) (*BitReader, error) {
+	// bufSize % chunkLen == 0 so that we never have to read across the buffer boundary
+	bs := &BitReader{chunkLen: chunkLen, in: in, buf: make([]byte, BufSize-BufSize%int(chunkLen))}
 	var err error
 	bs.bufN, err = bs.in.Read(bs.buf)
 	if err != nil {
@@ -36,92 +37,94 @@ func NewBitReader(chunkLen int, in io.Reader) (*BitReader, error) {
 }
 
 func (br *BitReader) Read() (byte, error) {
-	byteNum := br.bitIdx / 8
-	bitNum := br.bitIdx % 8
-	if byteNum >= br.bufN { // need to read more
+	if br.byteIdx >= br.bufN { // need to read more
 		n, err := br.in.Read(br.buf)
 		if err != nil {
 			return 0, err
 		}
-		br.bitIdx = bitNum
-		byteNum = br.bitIdx / 8
-		bitNum = br.bitIdx % 8
+		br.byteIdx = 0
 		br.bufN = n
 	}
 
 	var result byte
-	if bitNum+br.chunkLen > 8 { // want to slice past current byte
-		firstByte := sliceByteLen(br.buf[byteNum], bitNum, 8-bitNum)
-		secondPartLen := br.chunkLen + bitNum - 8
-		result = (firstByte << secondPartLen) + sliceByteLen(br.buf[byteNum+1], 0, secondPartLen)
-		br.bitIdx += br.chunkLen
-		return result, nil
+	if br.bitIdx+br.chunkLen > 8 { // want to slice past current byte
+		firstByte := sliceByteLen(br.buf[br.byteIdx], br.bitIdx, 8-br.bitIdx)
+		secondPartLen := br.chunkLen + br.bitIdx - 8
+		result = (firstByte << secondPartLen) + sliceByteLen(br.buf[br.byteIdx+1], 0, secondPartLen)
+	} else {
+		result = sliceByteLen(br.buf[br.byteIdx], br.bitIdx, br.chunkLen)
 	}
-	result = sliceByteLen(br.buf[byteNum], bitNum, br.chunkLen)
 	br.bitIdx += br.chunkLen
+	if br.bitIdx >= 8 {
+		br.bitIdx %= 8
+		br.byteIdx++
+	}
 	return result, nil
 }
 
 type BitWriter struct {
-	chunkLen int
+	chunkLen uint8
 	out      io.Writer
 
-	buf    []byte
-	bitIdx int
+	buf     []byte
+	bitIdx  uint8
+	byteIdx int
 }
 
-func NewBitWriter(chunkLen int, out io.Writer) *BitWriter {
-	//bufSize % bw.chunkLen == 0 so that we never have to write across the buffer boundary
-	return &BitWriter{chunkLen: chunkLen, out: out, buf: make([]byte, BufSize-BufSize%chunkLen)}
+func NewBitWriter(chunkLen uint8, out io.Writer) *BitWriter {
+	//bufSize % chunkLen == 0 so that we never have to write across the buffer boundary
+	return &BitWriter{chunkLen: chunkLen, out: out, buf: make([]byte, BufSize-BufSize%int(chunkLen))}
 }
 
 func (bw *BitWriter) Write(b byte) error {
-	bitNum := bw.bitIdx % 8
-	byteNum := bw.bitIdx / 8
-	if byteNum >= len(bw.buf) {
+	if bw.byteIdx >= len(bw.buf) {
 		_, err := bw.out.Write(bw.buf)
 		if err != nil {
 			return err
 		}
-		bw.buf = make([]byte, BufSize-BufSize%bw.chunkLen)
+		//fmt.Println("wrote ", len(bw.buf), string(bw.buf))
+		bw.buf = make([]byte, BufSize-BufSize%int(bw.chunkLen))
 		bw.bitIdx = 0
-		bitNum = 0
-		byteNum = 0
+		bw.byteIdx = 0
 	}
 
-	if bitNum+bw.chunkLen > 8 { // write across byte boundary?
+	if bw.bitIdx+bw.chunkLen > 8 { // write across byte boundary?
 		// 8-bw.chunkLen is where b's actual data starts from.
 		bStart := 8 - bw.chunkLen
 		// space left in current byte
-		left := 8 - bitNum
+		left := 8 - bw.bitIdx
 
-		bw.buf[byteNum] = bw.buf[byteNum] + sliceByteLen(b, bStart, left)
-		// bStart + left is up to where b has been read from. (bw.chunkLen+bitNum) - 8 is how many bits go to the next byte.
-		bw.buf[byteNum+1] = sliceByteLen(b, bStart+left, bw.chunkLen-left) << (bStart + left) // simplified 8 - (bw.chunkLen + bitNum - 8)
+		bw.buf[bw.byteIdx] = bw.buf[bw.byteIdx] + sliceByteLen(b, bStart, left)
+		// bStart + left is up to where b has been read from. (bw.chunkLen+br.bitIdx) - 8 is how many bits go to the next byte.
+		bw.buf[bw.byteIdx+1] = sliceByteLen(b, bStart+left, bw.chunkLen-left) << (bStart + left) // simplified 8 - (bw.chunkLen + br.bitIdx - 8)
 	} else {
-		bw.buf[byteNum] = bw.buf[byteNum] + (b << (8 - (bitNum + bw.chunkLen)))
+		bw.buf[bw.byteIdx] = bw.buf[bw.byteIdx] + (b << (8 - (bw.bitIdx + bw.chunkLen)))
 	}
 	bw.bitIdx += bw.chunkLen
+	if bw.bitIdx >= 8 {
+		bw.bitIdx %= 8
+		bw.byteIdx++
+	}
 	return nil
 }
 
 // Flush writes the rest of the buffer. Only call this at the end of the stream.
 func (bw *BitWriter) Flush() error {
-	_, err := bw.out.Write(bw.buf[:bw.bitIdx/8])
+	_, err := bw.out.Write(bw.buf[:bw.byteIdx])
 	return err
 }
 
 type Coding struct {
 	charset   []rune
-	numOfBits int
+	numOfBits uint8
 }
 
 // NewCoding creates a new Coding with the given character set. The length of the character set must be a power of 2
 // and must not contain duplicate runes.
 func NewCoding(charset []rune) (*Coding, error) {
-	numOfBits := int(math.Log2(float64(len(charset))))
+	numOfBits := uint8(math.Log2(float64(len(charset))))
 	if 1<<numOfBits != len(charset) {
-		numOfBits = int(math.Round(math.Log2(float64(len(charset)))))
+		numOfBits = uint8(math.Round(math.Log2(float64(len(charset)))))
 		return nil, errors.New(
 			fmt.Sprintln("charset length is not a power of two.\n   have:", len(charset),
 				"\n   want: a power of 2 (nearest is", 1<<numOfBits, "which is", math.Abs(float64(len(charset)-1<<numOfBits)), "away)"),
